d7df74620704593e3b7db66cf8dce115367914e7
JingYu-Pilot <jyzl.18996134580@gmail.com>
2022-03-26 11:54:36 +0800

    Initial commit

---
+++ question-service/src/main/java/com/jingyu/question/QuestionServiceBootstrap.java
@@ -0,0 +1,13 @@
+package com.jingyu.question;
+
+/**
+ * TODO: <p> ApplicationBootstrap </p>
+ *
+ * @author: JingYu
+ * @create: 2022-03-26
+ */
+public class QuestionServiceBootstrap {
+    public static void main(String[] args) {
+        System.out.println("\"HelloWorld\" = " + "HelloWorld");
+    }
+}
---
+++ question-service/src/test/java/com/jingyu/question/SmokeTest.java
@@ -0,0 +1,18 @@
+package com.jingyu.question;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * TODO: <p> TestApplicationRunnable </p>
+ * 冒烟测试
+ * @author: JingYu
+ * @create: 2022-03-26
+ */
+public class SmokeTest {
+    @Test
+    void one_plus_one_equals_two() {
+        assertEquals(2,1+1);
+    }
+}
